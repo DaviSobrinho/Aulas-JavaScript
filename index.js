45b07c38dfd85dc129fdc719d80fd1b84dd2393d
Davi Sobrinho Labre <102126095+DaviSobrinho@users.noreply.github.com>
2023-05-29 21:01:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ app.set('view engine','html')
 app.set('views', __dirname + '/views')
 
 
+app.post('/form', function(req, res){
+    res.render('form.html', {contato})
+    
+})
 app.get('', function (req, res){
     res.render('index.html')
+    let contato = {
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        mensagem:mensagem
+    }
 })
 
 const app_port = 8080
